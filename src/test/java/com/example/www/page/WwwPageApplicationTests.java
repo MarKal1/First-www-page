836e573cc353d6e84520d7b113a47258d6cde7c4
Martin Kalinowski <74590534+MarKal1@users.noreply.github.com>
2021-01-02 17:19:28 +0100

    General Project input

---
+++ src/test/java/com/example/www/page/WwwPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.www.page;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WwwPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
